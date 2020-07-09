4f1d05669f9f9385e726aed9ec5351e4d71f8828
Serhii Hayvoronskyi <hayvoronskyi@ukr.net>
2020-07-10 02:22:47 +0300

    v1

--- js/slider.js
+++ js/slider.js
@@ -1,13 +1,42 @@
-(() => {
-  const menuBtnRef = document.querySelector('[data-menu-button]');
-  const mobileMenuRef = document.querySelector('[data-menu]');
+let position = 0;
+const slidersToShow = 1;
+const slidersToScroll = 1;
+const container = document.querySelector('.slider-container');
+const track = document.querySelector('.slider-track');
+const items = document.querySelectorAll('.slider-item');
+const btnPrev = document.querySelector('.btn-prev');
+const btnNext = document.querySelector('.btn-next');
+const itemsCount = items.length;
+const itemWidth = container.clientWidth / slidersToShow;
+const movePosition = slidersToScroll * itemWidth;
 
-  menuBtnRef.addEventListener('click', () => {
-    const expanded = menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+items.forEach(item => {
+  item.style.minWidth = `${itemWidth}px`;
+});
 
-    menuBtnRef.classList.toggle('is-open');
-    menuBtnRef.setAttribute('aria-expanded', !expanded);
+btnNext.addEventListener('click', () => {
+  const itemsLeft = itemsCount - (Math.abs(position) + slidersToShow * itemWidth) / itemWidth;
+  position -= itemsLeft >= slidersToScroll ? movePosition : itemsLeft * itemWidth;
 
-    mobileMenuRef.classList.toggle('is-open');
-  });
-})();
+  setPosition();
+  checkBtns();
+});
+
+btnPrev.addEventListener('click', () => {
+  const itemsLeft = Math.abs(position) / itemWidth;
+  position += itemsLeft >= slidersToScroll ? movePosition : itemsLeft * itemWidth;
+
+  setPosition();
+  checkBtns();
+});
+
+const setPosition = () => {
+  track.style.transform = `translateX(${position}px)`;
+};
+
+const checkBtns = () => {
+  btnPrev.disabled = position === 0;
+  btnNext.disabled = position <= -(itemsCount - slidersToShow) * itemWidth;
+};
+
+// checkBtns();
